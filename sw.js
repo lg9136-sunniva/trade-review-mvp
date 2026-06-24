1ca9032eec46862f77338a515f14721661bb16eb
lg9136-sunniva <lg9136@gmail.com>
2026-06-24 15:39:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "trade-review-mvp-v1";
+const CACHE_NAME = "trade-review-mvp-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
